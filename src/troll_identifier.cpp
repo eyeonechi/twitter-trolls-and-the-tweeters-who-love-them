76e0044720e312491083d0ba9a6a6711973f0d4e
Ivan Ken Weng Chee <ichee@student.unimelb.edu.au>
2018-09-21 19:56:36 +1000

    initialised project structure; added driver program

---
+++ src/troll_identifier.cpp
@@ -0,0 +1,21 @@
+/*
+ * File    : troll_identifier.cpp
+ * Title   : Twitter Trolls and the Tweeters who Love them
+ * Author  : 736901
+ * Created : 21/09/2018
+ * Purpose : COMP90049 2018S2 Assignment 2
+ */
+
+ /* Main function */
+ int main(int argc, char **argv) {
+
+   // Start time
+   clock_t tick, tock;
+   tick = clock();
+
+   // End time
+   tock = clock();
+   float diff = ((float) tock - (float) tick) / CLOCKS_PER_SEC;
+   cout << "Time: " << diff << "s" << endl;
+
+ }
